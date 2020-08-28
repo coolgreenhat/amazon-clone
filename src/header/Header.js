7dc75fef3af318b18f90db7a3b6b63b710bb3b58
sanket <sanketgargate06@gmail.com>
2020-08-28 21:31:16 +0530

    added header component and styling

---
+++ src/header/Header.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom';
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+
+function Header() {
+  return (
+    <nav className="header">
+      {/* logo on the left */}
+      <Link to="/">
+          <img className="header__logo" 
+          src="https://pngimg.com/uploads/amazon/amazon_PNG25.png" 
+          alt=""/>
+      </Link>  
+      {/* Search */}
+      <div className="header__search">
+        <input type="text" className="header__searchInput"/>
+        <SearchIcon className="header__searchIcon" />
+      </div>
+  
+      {/* 3 links */}
+      <div className="header__nav">
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello User</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+
+         {/* basket icon */}
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            <ShoppingCartIcon />
+            <span className="header__optionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
+
+      </div>
+     
+    </nav>
+  )
+}
+
+export default Header;
