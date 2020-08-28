888ae4ffb5d0ca6cd694203da01c11f4056455b4
sanket <sanketgargate06@gmail.com>
2020-08-28 22:57:32 +0530

    added banner

---
+++ src/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Home.css"
+
+
+function Home() {
+  return (
+    <div className="home">
+      <img 
+      className="home__image"
+      src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/AmazonDevices/2019/Post_AugArt/1x_Dot._CB406062798_.jpg" 
+      alt=""/>
+    </div>
+  )
+}
+
+export default Home
