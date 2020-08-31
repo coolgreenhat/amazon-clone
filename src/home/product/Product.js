0f28998972e922ccf9a0f0293d263c7b73a7ba25
sanket <sanketgargate06@gmail.com>
2020-08-31 20:14:07 +0530

    added functionality to addTobasket button

--- src/home/product/Product.js
+++ src/home/product/Product.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import "./Product.css";
+import { useStateValue } from "../../StateProvider";
 
 function Product({id, title, image, price, rating }) {
+
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
         <p>{title}</p>
         <p className="product__price">
-          <large>₹ </large>
+          <span>₹ </span >
           <strong>{price}</strong>
         </p>
         <div className="product__rating">
@@ -22,7 +39,7 @@ function Product({id, title, image, price, rating }) {
       </div>
       
       <img src={image} alt=""/>
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   )
 }
