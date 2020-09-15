f0bdc355fc479f729bfe74159a4975dc9300c51d
sanket <sanketgargate06@gmail.com>
2020-09-15 22:07:12 +0530

    added login page and functionality

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link, useHistory } from "react-router-dom";
+import { auth } from "../firebase";
 
 function Login() {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = e => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(auth => {
+        history.push('/');
+        console.log('Logged In');
+      })
+      .catch(error => alert(error.message))
+  }
+
+  const register = e => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        console.log('Created User');
+        console.log(auth);
+        if (auth) {
+          history.push('/')
+        }
+      })
+      .catch(error => alert(error.message))
+  }
+
   return (
     <div className="login">
       <Link to="/">
@@ -17,16 +49,16 @@ function Login() {
         <h1>Sign In</h1>
         <form>
           <h5>Email</h5>
-          <input type="text" />
+          <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
           <h5>Password</h5>
-          <input type="text" />
-          <button>Sign In</button>
+          <input type="password"  value={password} onChange={e => setPassword(e.target.value)} />
+          <button onClick={signIn} className='login__signInButton'>Sign In</button>
         </form>
         <p>
           By continuing, you agree to Amazon's Conditions of Use and Privacy
           Notice.
         </p>
-        <button>Create Your Amazon Account</button>
+        <button onClick={register} type='submit' className='login__registerButton'>Create Your Amazon Account</button>
       </div>
     </div>
   );
